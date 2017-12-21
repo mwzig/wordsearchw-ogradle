d44ab379da7e24b45d37aefb899c866b0975aca3
mwzig <mwziegler2014@gmail.com>
2017-12-20 20:10:21 -0500

    iteration 2 - testing one row l to r with commas in between chars

--- src/WordSearch.java
+++ src/WordSearch.java
@@ -7,18 +7,25 @@ public class WordSearch {
 	}
 	
 	private char[] inputRow;
-	
+	private String inputRowString;
 	private String wordToFind;
 	
 	public WordSearch(char[] inputRow, String wordToFind) {
 	
 		this.inputRow = inputRow;
 		this.wordToFind = wordToFind;
+		// The input row has a comma between each character.
+		// We need to parse out the commas.
+		inputRowString = "";
+		for (int i = 0; i < inputRow.length; i++) {
+			if (inputRow[i] != ',') {
+				inputRowString += inputRow[i];
+			}
+		}
 	}
 	
 	public boolean findWord() {
-		String row = new String(inputRow);
-		if (row.contains(wordToFind)) {
+		if (inputRowString.contains(wordToFind)) {
 			return true;
 		}
 		else {
--- src/WordSearchTest.java
+++ src/WordSearchTest.java
@@ -4,9 +4,42 @@ import org.junit.Test;
 
 public class WordSearchTest {
 
+	// Iteration 1 - started with a WordSearch constructor that 
+	// just took in chars like this 
+	/*
 	@Test
 	public void findWordLtoRInOneRowTrue() {
 		char rowChars[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+		
+		WordSearch wordSearch = new WordSearch(rowChars, "bcd");
+		boolean valueToCheck = wordSearch.findWord(); 
+		assertTrue(valueToCheck);
+	}
+
+	@Test
+	public void findWordLtoRInOneRowFalse() {
+		char rowChars[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+		
+		WordSearch wordSearch = new WordSearch(rowChars, "bqd");
+		boolean valueToCheck = wordSearch.findWord(); 
+		assertFalse(valueToCheck);
+	}
+	*/
+	
+	
+	// Iteration 2 - started with a WordSearch constructor that 
+	// just took in chars with a comma in between each 
+	
+	@Test
+	public void findWordLtoRInOneRowTrue() {
+		char rowChars[] = {'a', ',', 
+				           'b', ',',
+				           'c', ',',
+				           'd', ',',
+				           'e', ',',
+				           'f', ',',
+				           'g', ',',
+				           'h' };
 		
 		WordSearch wordSearch = new WordSearch(rowChars, "bcd");
 		boolean valueToCheck = wordSearch.findWord(); 
