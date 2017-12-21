6d9bb84039439d5b515cfd8b615e6c1cfe4b37ee
mwzig <mwziegler2014@gmail.com>
2017-12-20 19:55:50 -0500

    initial test works

---
+++ src/WordSearchTest.java
@@ -0,0 +1,25 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class WordSearchTest {
+
+	@Test
+	public void findWordLtoRInOneRowTrue() {
+		char rowChars[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+		
+		WordSearch wordSearch = new WordSearch(rowChars, "bcd");
+		boolean valueToCheck = wordSearch.findWord(); 
+		assertTrue(valueToCheck);
+	}
+
+	@Test
+	public void findWordLtoRInOneRowFalse() {
+		char rowChars[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' };
+		
+		WordSearch wordSearch = new WordSearch(rowChars, "bqd");
+		boolean valueToCheck = wordSearch.findWord(); 
+		assertFalse(valueToCheck);
+	}
+
+}
