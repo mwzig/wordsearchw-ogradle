7ad0f555bd22a40aed47f32df831bd3f3d9470ef
mwzig <mwziegler2014@gmail.com>
2017-12-26 17:18:33 -0500

    test works for WordSearchTest diagonal top left bottom right

--- src/test/WordSearchTest.java
+++ src/test/WordSearchTest.java
@@ -27,28 +27,45 @@ public class WordSearchTest {
 
 			int xCoordinate = 0; 
 			locCoordinate = new LocCoordinate(xCoordinate, 0);
-			gridLetter = new GridLetter('c', locCoordinate);
+			gridLetter = new GridLetter('a', locCoordinate);
 			inputGrid[xCoordinate][0] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 1);
-			gridLetter = new GridLetter('a', locCoordinate);
+			gridLetter = new GridLetter('b', locCoordinate);
 			inputGrid[xCoordinate][1] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 2);
-			gridLetter = new GridLetter('t', locCoordinate);
+			gridLetter = new GridLetter('c', locCoordinate);
 			inputGrid[xCoordinate][2] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 3);
-			gridLetter = new GridLetter('s', locCoordinate);
+			gridLetter = new GridLetter('d', locCoordinate);
 			inputGrid[xCoordinate][3] = gridLetter;
 		
 			xCoordinate = 1;
 			locCoordinate = new LocCoordinate(xCoordinate, 0);
-			gridLetter = new GridLetter('o', locCoordinate);
+			gridLetter = new GridLetter('e', locCoordinate);
 			inputGrid[xCoordinate][0] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 1);
-			gridLetter = new GridLetter('o', locCoordinate);
+			gridLetter = new GridLetter('f', locCoordinate);
+			inputGrid[xCoordinate][1] = gridLetter;
+
+			locCoordinate = new LocCoordinate(xCoordinate, 2);
+			gridLetter = new GridLetter('g', locCoordinate);
+			inputGrid[xCoordinate][2] = gridLetter;
+
+			locCoordinate = new LocCoordinate(xCoordinate, 3);
+			gridLetter = new GridLetter('h', locCoordinate);
+			inputGrid[xCoordinate][3] = gridLetter;
+		
+			xCoordinate = 2;
+			locCoordinate = new LocCoordinate(xCoordinate, 0);
+			gridLetter = new GridLetter('i', locCoordinate);
+			inputGrid[xCoordinate][0] = gridLetter;
+
+			locCoordinate = new LocCoordinate(xCoordinate, 1);
+			gridLetter = new GridLetter('j', locCoordinate);
 			inputGrid[xCoordinate][1] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 2);
@@ -56,41 +73,24 @@ public class WordSearchTest {
 			inputGrid[xCoordinate][2] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 3);
-			gridLetter = new GridLetter('g', locCoordinate);
-			inputGrid[xCoordinate][3] = gridLetter;
-		
-			xCoordinate = 2;
-			locCoordinate = new LocCoordinate(xCoordinate, 0);
-			gridLetter = new GridLetter('d', locCoordinate);
-			inputGrid[xCoordinate][0] = gridLetter;
-
-			locCoordinate = new LocCoordinate(xCoordinate, 1);
-			gridLetter = new GridLetter('a', locCoordinate);
-			inputGrid[xCoordinate][1] = gridLetter;
-
-			locCoordinate = new LocCoordinate(xCoordinate, 2);
-			gridLetter = new GridLetter('a', locCoordinate);
-			inputGrid[xCoordinate][2] = gridLetter;
-
-			locCoordinate = new LocCoordinate(xCoordinate, 3);
-			gridLetter = new GridLetter('o', locCoordinate);
+			gridLetter = new GridLetter('l', locCoordinate);
 			inputGrid[xCoordinate][3] = gridLetter;
 		
 			xCoordinate = 3;
 			locCoordinate = new LocCoordinate(xCoordinate, 0);
-			gridLetter = new GridLetter('y', locCoordinate);
+			gridLetter = new GridLetter('m', locCoordinate);
 			inputGrid[xCoordinate][0] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 1);
-			gridLetter = new GridLetter('p', locCoordinate);
+			gridLetter = new GridLetter('n', locCoordinate);
 			inputGrid[xCoordinate][1] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 2);
-			gridLetter = new GridLetter('i', locCoordinate);
+			gridLetter = new GridLetter('o', locCoordinate);
 			inputGrid[xCoordinate][2] = gridLetter;
 
 			locCoordinate = new LocCoordinate(xCoordinate, 3);
-			gridLetter = new GridLetter('d', locCoordinate);
+			gridLetter = new GridLetter('p', locCoordinate);
 			inputGrid[xCoordinate][3] = gridLetter;
 		
 
@@ -111,15 +111,16 @@ public class WordSearchTest {
 	@Test
 	public void FindWordsAcross() {
 		ArrayList<String> wordsToFind = new ArrayList<String>();
-		wordsToFind.add("cats");
-		wordsToFind.add("okg");
-		wordsToFind.add("daa");
-		wordsToFind.add("pi");
+		wordsToFind.add("abcd");
+		wordsToFind.add("fgh");
+		wordsToFind.add("ijk");
+		wordsToFind.add("no");
 		
 		Grid letterGrid = new Grid(inputGrid);
 		WordSearch wordSearch = new WordSearch(letterGrid, wordsToFind);
 		boolean bAllWordsFound = wordSearch.findWords();
 		for (FoundWord foundWord: wordSearch.getFoundWords()) {
+			System.out.println("found words down");
 			System.out.println(foundWord.toString());
 		}
 		assertTrue(bAllWordsFound);
@@ -127,15 +128,34 @@ public class WordSearchTest {
 	@Test
 	public void FindWordsDown() {
 		ArrayList<String> wordsToFind = new ArrayList<String>();
-		wordsToFind.add("ody");
-		wordsToFind.add("oap");
-		wordsToFind.add("tk");
-		wordsToFind.add("sgod");
+		wordsToFind.add("aeim");
+		wordsToFind.add("fjn");
+		wordsToFind.add("gk");
+		wordsToFind.add("dh");
 		
 		Grid letterGrid = new Grid(inputGrid);
 		WordSearch wordSearch = new WordSearch(letterGrid, wordsToFind);
 		boolean bAllWordsFound = wordSearch.findWords();
 		for (FoundWord foundWord: wordSearch.getFoundWords()) {
+			System.out.println("found words across");
+			System.out.println(foundWord.toString());
+		}
+		assertTrue(bAllWordsFound);
+	}
+	@Test
+	public void FindWordsDiagonalTopLeftBottomRight() {
+		ArrayList<String> wordsToFind = new ArrayList<String>();
+		wordsToFind.add("eb");
+		wordsToFind.add("ifc");
+		wordsToFind.add("mjgd");
+		wordsToFind.add("nkh");
+		wordsToFind.add("ol");
+			
+		Grid letterGrid = new Grid(inputGrid);
+		WordSearch wordSearch = new WordSearch(letterGrid, wordsToFind);
+		boolean bAllWordsFound = wordSearch.findWords();
+		for (FoundWord foundWord: wordSearch.getFoundWords()) {
+			System.out.println("found words diagonally top left bottom right");
 			System.out.println(foundWord.toString());
 		}
 		assertTrue(bAllWordsFound);
